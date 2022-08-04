2251fd7d1bdb822da57c6cd5961623d7a2761fe5
elham poshtiban <elham@londr.com>
2022-08-03 22:00:49 -0500

    I learned array and for loop

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var doom=0; doom<topics.length;doom++){
+    console.log(topics[doom])
+}
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
+
+
